bbd5ffb8ba18ef678fa6ef54750c199dd376763c
sohyeon <sohyun9743@naver.com>
2026-08-19 00:28:40 +0900

    AOS.js 라이브러리로 모션 구현

--- js/script.js
+++ js/script.js
@@ -10,25 +10,21 @@ $(document).ready(function () {
       var targetBottom = targetTop + $(this).outerHeight();
       var targetId = $(this).attr('id');
 
-      // 현재 스크롤 위치가 해당 section 범위 내에 들어왔을 때
       if (scrollPos >= targetTop && scrollPos < targetBottom) {
-        $('.navi ul li a').removeClass('active'); // 전체 active 제거
-        $('.navi ul li a[href="#' + targetId + '"]').addClass('active'); // 해당 메뉴만 active 추가
+        $('.navi ul li a').removeClass('active'); 
+        $('.navi ul li a[href="#' + targetId + '"]').addClass('active');
       }
     });
   });
 
-  // 앵커 클릭 시 부드럽게 이동하는 효과 (선택 사항)
   $('.navi ul li a').on('click', function (e) {
     var target = $(this).attr('href');
-    
-    // #으로 시작하는 내부 링크일 경우에만 작동
     if (target.startsWith('#')) {
       e.preventDefault();
       var targetTop = $(target).offset().top;
 
       $('html, body').animate({
-        scrollTop: targetTop - 70 // 헤더 높이 고려하여 이동
+        scrollTop: targetTop - 60
       }, 400);
     }
   });
@@ -37,8 +33,7 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   var $videoWrapper = $('.video_wrapper');
-  // jQuery 객체에서 실제 HTML5 Video DOM 요소[0]를 추출해야 .play(), .pause()가 작동합니다.
-  var video = $('#myVideo')[0]; 
+  var video = $('#videoBox')[0]; 
 
   $videoWrapper.on('click', function () {
     if (video.paused) {
@@ -51,7 +46,7 @@ $(document).ready(function () {
   });
 
   // 영상이 끝까지 재생되었을 때 처리
-  $('#myVideo').on('ended', function () {
+  $('#videoBox').on('ended', function () {
     $videoWrapper.removeClass('is-playing');
   });
 });
